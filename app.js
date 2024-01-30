938daa3aad38740767570b53cdbef7c397a42776
Johnny Casares <88862676+JohnnyCasares@users.noreply.github.com>
2024-01-29 22:02:48 -0500

    fixed iso request

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const { connectToDB, getDB } = require("./db");
+const { Int32 } = require("mongodb");
 
 const app = express();
 app.use(express.json()); //pass any json coming from a request so we can use it in handler functions
@@ -23,12 +24,12 @@ app.get(
     "/countries",
     (req, res) => {
         //TODO implement pagination
-        const pageNum = req.query.page ?? 0; 
+        const pageNum = req.query.page ?? 0;
         const docsPerPage = 34;
         countries = [];
         countriesCollection
             .find()
-            .skip(docsPerPage*pageNum)
+            .skip(docsPerPage * pageNum)
             .limit(docsPerPage)
             .forEach(country => countries.push(country))
             .then(() => {
@@ -39,46 +40,43 @@ app.get(
                 res.status(500).json({ error: 'Query failed' });
             });
     });
-
 //get by iso alpha 3 or alpha 2
 app.get(
-    "/countries/:alpha",
-    (req, res)=>{
-        const iso = req.params.alpha.toLowerCase();
-        if(iso.length == 2){
+    "/countries/:iso",
+    (req, res) => {
+        const iso = req.params.iso.toLowerCase();
+        let isnum = /^\d+$/.test(iso);
+        if (isnum) {
+            // console.log('hello from the if statement xd');
+            numeric = new Int32(Number(iso));
             countriesCollection
-            .findOne({alpha2:iso})
-            .then((country)=>{res.status(200).json(country)})
-            .catch((err)=>res.status(500).json({error: 'Could not find document'}));
-            
-        }
-        else if(iso.length == 3){
-            countriesCollection
-            .findOne({alpha3:iso})
-            .then((country)=>{res.status(200).json(country)})
-            .catch((err)=>res.status(500).json({error: 'Could not find document'}));
-            
-        }else{
-            res.status(500).json({error:'The official ISO 4217 standard specifies three-letter ("Alpha-3") codes for currencies worldwide'})
+                .findOne({ numeric: numeric })
+                .then((country) => { res.status(200).json(country) })
+                .catch((err) => res.status(500).json({ error: 'Could not find document' }));
+        } else {
+            if (iso.length == 2) {
+                countriesCollection
+                    .findOne({ alpha2: iso })
+                    .then((country) => { res.status(200).json(country) })
+                    .catch((err) => res.status(500).json({ error: 'Could not find document' }));
+
+            }
+            else if (iso.length == 3) {
+                countriesCollection
+                    .findOne({ alpha3: iso })
+                    .then((country) => { res.status(200).json(country) })
+                    .catch((err) => res.status(500).json({ error: 'Could not find document' }));
+            } else {
+                res.status(500).json({ error: 'The official ISO 4217 standard specifies three-letter (Alpha-3) & two-letter (Alpha-2) codes for currencies worldwide' })
+            }
         }
 
 
     }
 
 );
+
+
 
 
 //developer endpoints
-
-//update many
-
-app.patch(
-    '/countries/:alpha3',
-    (req, res)=>{
-        
-    }
-);
-
-
-
-
